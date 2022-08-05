9a8e8a3ca03c54e106107b0affa9fdc318d9d55b
Heinz Wiesinger <heinz@m2mobi.com>
2022-08-05 16:48:38 +0200

    General: Add workaround for phpstan 1.7+

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -33,8 +33,6 @@ else
     require_once $base . '/decomposer.autoload.inc.php';
 }
 
-define('REFLECTION_BUG_72194', (PHP_MAJOR_VERSION > 5));
-
 if (defined('TEST_STATICS') === FALSE)
 {
     define('TEST_STATICS', __DIR__ . '/statics');
