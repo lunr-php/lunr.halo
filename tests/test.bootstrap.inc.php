fb521e2d130485a7f0d482665a1f0a4d44712952
Heinz Wiesinger <heinz@m2mobi.com>
2022-09-09 13:50:12 +0200

    General: Fix typo in comment in test bootstrap file

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -29,7 +29,7 @@ if (file_exists($base . '/vendor/autoload.php') == TRUE)
 }
 else
 {
-    // Load decomposer autoloade.
+    // Load decomposer autoloader.
     require_once $base . '/decomposer.autoload.inc.php';
 }
 
