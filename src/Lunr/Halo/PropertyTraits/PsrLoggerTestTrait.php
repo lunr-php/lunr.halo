3cb1bd1a5071b07ce0791c4a23d63c43965bf343
Heinz Wiesinger <heinz@m2mobi.com>
2023-02-08 22:18:59 +0100

    Traits: Make logger property name configurable
    Reviewed at https://reviews.lunr.nl/r/915/

--- src/Lunr/Halo/PropertyTraits/PsrLoggerTestTrait.php
+++ src/Lunr/Halo/PropertyTraits/PsrLoggerTestTrait.php
@@ -18,11 +18,17 @@ trait PsrLoggerTestTrait
 {
 
     /**
+     * Default property name for the logger
+     * @var string
+     */
+    private string $property_name_logger = 'logger';
+
+    /**
      * Test that the Logger class is passed correctly.
      */
     public function testLoggerIsSetCorrectly(): void
     {
-        $property = $this->get_reflection_property_value('logger');
+        $property = $this->get_reflection_property_value($this->property_name_logger);
 
         $this->assertSame($property, $this->logger);
         $this->assertInstanceOf('Psr\Log\LoggerInterface', $property);
