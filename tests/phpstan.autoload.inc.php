40b1d2a0435a13b1376c6ae2ddb679eaff238a7d
Heinz Wiesinger <heinz.wiesinger@moveagency.com>
2025-05-01 11:33:17 +0200

    General: Switch to camelCaps variables in test bootstrap files

--- tests/phpstan.autoload.inc.php
+++ tests/phpstan.autoload.inc.php
@@ -14,15 +14,15 @@ $base = __DIR__ . '/..';
 if (file_exists($base . '/vendor/autoload.php') == TRUE)
 {
     // Load composer autoloader.
-    $autoload_file = $base . '/vendor/autoload.php';
+    $autoloadFile = $base . '/vendor/autoload.php';
 }
 else
 {
     // Load decomposer autoloader.
-    $autoload_file = $base . '/decomposer.autoload.inc.php';
+    $autoloadFile = $base . '/decomposer.autoload.inc.php';
 }
 
-require_once $autoload_file;
+require_once $autoloadFile;
 
 if (file_exists($base . '/vendor/autoload.php') == FALSE)
 {
--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -25,15 +25,15 @@ set_include_path(
 if (file_exists($base . '/vendor/autoload.php') == TRUE)
 {
     // Load composer autoloader.
-    $autoload_file = $base . '/vendor/autoload.php';
+    $autoloadFile = $base . '/vendor/autoload.php';
 }
 else
 {
     // Load decomposer autoloader.
-    $autoload_file = $base . '/decomposer.autoload.inc.php';
+    $autoloadFile = $base . '/decomposer.autoload.inc.php';
 }
 
-require_once $autoload_file;
+require_once $autoloadFile;
 
 if (defined('TEST_STATICS') === FALSE)
 {
